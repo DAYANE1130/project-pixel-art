1aa2fba2628750b18d0971841c6b3bfce2a42fb7
Dayane <dayanebmartins@gmail.com>
2021-07-09 16:31:43 -0300

    adiciona questão 8

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// Questão 7
 
-//A classe selected deve ser adicionada à cor selecionada na paleta, ao mesmo tempo em que é removida da cor anteriormente selecionada;
-
-//Somente uma das cores da paleta deve ter a classe selected de cada vez;
-
-//Note que os elementos que deverão receber a classe selected devem ser os mesmos elementos que possuem a classe color, como especificado no requisito 2.
-
-//ANOTAÇÕES:
-
-// Primeiro selecionar  cor (COR  y selecionada = classe selected)
-// tenho q incluir condiçoes oara que a classe selected seja de somente uma das cores?
-// Como identificar a cor selecionada?
-// Usarei um evento para isso? Um click no mouse?
-// SE vou clicar ele tem que virar um botão? certo?
-// como ele vai preender o pixel no quadro ?(tipo identificando o quadro q o usuario escolheu com a cor selecionada fazendo a ligação entre elas.
-// esse evento tem q terminar para eu conseguir mudar de cor
 
  const selecionaCor= document.getElementById("color-palette");
-
+ let corDoMomento="black";
  function mudaCor(){
      selecionaCor.addEventListener('click',function(event){
          let corSelecionada=document.querySelector(".selected");
          corSelecionada.classList.remove("selected");
-         event.target.className +=" selected";
-     })
+         event.target.classList.add("selected");
+         corDoMomento=event.target.id;
+     }
+     )
     
  } 
  mudaCor();
+
+ 
+function colorPixel(){
+    let pixel = document.querySelectorAll('.pixel');
+    let color = document.querySelectorAll('.color');
+    
+    for(let index= 0; index < pixel.length; index += 1){
+    pixel[index].addEventListener('click', function(){
+    pixel[index].style.backgroundColor = corDoMomento; 
+    });
+    }
+    }
+    colorPixel(); 
+
+
